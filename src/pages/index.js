6dbb454b3f4a09e5616ccab4e803c0dcc59bb123
vamsigangadhar <vamsigangadhar@bonamisoftware.com>
2021-06-24 16:47:30 +0530

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Layout from "./Layout";
-// import { useState, useEffect } from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import { useState, useEffect } from "react";
 
 const IndexPage = () => {
   // const [data, setData] = useState([]);
@@ -14,6 +15,10 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
 
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/gatsby.webp"
+      />
       {/* <ul>
         {data.map((post, id) => {
           return (
