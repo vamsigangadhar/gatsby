e482f90a903f855878815806f1ec0ad093f0d3cb
vamsigangadhar <vamsigangadhar@bonamisoftware.com>
2021-06-25 10:59:10 +0530

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsbay Site",
+    title: "My First Gatsbay Site renamed",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
   ],
 };
